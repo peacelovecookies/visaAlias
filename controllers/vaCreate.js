822cbc95cf4de5e3423852ab6fd113f42d939598
Pavel Kim <Pavel.Kim@ipotekabank.uz>
2023-01-16 18:03:12 +0500

    remove testing row

--- controllers/vaCreate.js
+++ controllers/vaCreate.js
@@ -5,8 +5,8 @@ import { decryptPan } from "../utils/crypto.js";
 // Create https://developer.visa.com/capabilities/visa_direct/reference#tag/Visa-Alias-Directory-API/operation/Create%20Alias_v1
 export default async (req, res) => {
     const { body } = req;
-    let recipientPrimaryAccountNumber = decryptPan(body.recipientPrimaryAccountNumber);
-    recipientPrimaryAccountNumber = ('9' + recipientPrimaryAccountNumber).slice(0, 15); // remove this sh*t in production!!!
+    const recipientPrimaryAccountNumber = decryptPan(body.recipientPrimaryAccountNumber);
+    // recipientPrimaryAccountNumber = ('9' + recipientPrimaryAccountNumber).slice(0, 15); // remove this sh*t in production!!!
     const createPayload = {
         consentDateTime: getCurrentDate(), // UTC Format: YYYY-MM-DD hh:mm:ss
         aliasType: '01',
